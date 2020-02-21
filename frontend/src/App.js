5fc5e15e2a4a2ff77a3e985ffa3654ad07de149f
ilya <ilyakodit@mail.ru>
2020-02-22 00:23:28 +0300

    Login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import {BrowserRouter as Router} from 'react-router-dom';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 import 'antd/dist/antd.css';
-import './style/style.css'
+import './style/style.css';
 
 import Navbar from "./components/Navbar";
+import Login from "./components/Login";
+import Home from "./components/Home";
 
 class App extends Component {
 
@@ -15,6 +17,11 @@ class App extends Component {
                 <div>
 
                     <Navbar />
+
+                    <Switch>
+                        <Route exact path={'/'} component={Home}/>
+                        <Route path={'/login'} component={Login}/>
+                    </Switch>
 
                 </div>
             </Router>
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,4 +1,6 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {Link} from 'react-router-dom';
 
 import {Icon, Menu} from "antd";
 
@@ -19,44 +21,47 @@ class Navbar extends Component {
 
     render() {
         return (
-            <div>
+            <div className={'navbar_cont'}>
 
-                <Menu onClick={this.handleClick} selectedKeys={[this.state.current]} mode="horizontal"
-                      className={'navbar_cont'}>
+                <div>
+                    <Menu onClick={this.handleClick} selectedKeys={[this.state.current]} mode="horizontal"
+                          className={'navbar_left'}>
+                        <Menu.Item key="home">
+                            <Icon type="home"/>
+                            <Link to={'/'} className={'navbar_link'}>Главная</Link>
+                        </Menu.Item>
+                        <SubMenu
+                            title={
+                                <span className="submenu-title-wrapper">
+                                    <Icon type="bars"/>
+                                    Инструменты
+                                </span>
+                            }
+                        >
+                            <Menu.ItemGroup title="Отправка грузов">
+                                <Menu.Item key="findCarrier">Найти грузоперевочика</Menu.Item>
+                                <Menu.Item key="trackShipment">Отследить отправку</Menu.Item>
+                            </Menu.ItemGroup>
+                            <Menu.ItemGroup title="Дополнительно">
+                                <Menu.Item key="volumeCalc">Калькулятор объёма груза</Menu.Item>
+                            </Menu.ItemGroup>
+                        </SubMenu>
+                    </Menu>
+                </div>
 
-                    <Menu.Item key="home">
-                        <Icon type="home"/>
-                        Главная
-                    </Menu.Item>
-
-                    <SubMenu
-                        title={
-                            <span className="submenu-title-wrapper">
-                                <Icon type="bars"/>
-                                Инструменты
-                            </span>
-                        }
-                    >
-                        <Menu.ItemGroup title="Отправка грузов">
-                            <Menu.Item key="findCarrier">Найти грузоперевочика</Menu.Item>
-                            <Menu.Item key="trackShipment">Отследить отправку</Menu.Item>
-                        </Menu.ItemGroup>
-                        <Menu.ItemGroup title="Дополнительно">
-                            <Menu.Item key="volumeCalc">Калькулятор объёма груза</Menu.Item>
-                        </Menu.ItemGroup>
-                    </SubMenu>
-
-                    <Menu.Item key="login">
-                        <Icon type="user"/>
-                        Войти
-                    </Menu.Item>
-
-
-                </Menu>
+                <div>
+                    <Menu onClick={this.handleClick} selectedKeys={[this.state.current]} mode="horizontal"
+                          className={'navbar_right'}>
+                        <Menu.Item key="login">
+                            <Icon type="user"/>
+                            <Link to={'/login'} className={'navbar_link'}>Войти</Link>
+                        </Menu.Item>
+                    </Menu>
+                </div>
 
             </div>
         );
     }
 }
 
-export default Navbar;
+export default connect()(Navbar);
