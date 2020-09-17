2b36ddfda5c7b609afb3bb89bccd92bff8e86bb5
ilyaMah <ilya.unica@gmail.com>
2020-09-17 21:42:08 +0300

    yandex metrika

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,71 +1,78 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
+import { YMInitializer } from 'react-yandex-metrika';
 
 import { Form, Icon, Input, Button, Checkbox, Card } from 'antd';
 import {Link} from "react-router-dom";
 
 class Login extends Component {
 
-    handleSubmit = e => {
-        e.preventDefault();
-        this.props.form.validateFields((err, values) => {
-            if (!err) {
-                console.log('Received values of form: ', values);
-            }
-        });
-    };
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.form.validateFields((err, values) => {
+        if (!err) {
+            console.log('Received values of form: ', values);
+        }
+    });
+  };
 
-    render() {
-        const { getFieldDecorator } = this.props.form;
-        return (
+  render() {
+    const { getFieldDecorator } = this.props.form;
+    return (
+      <div className={'login_cont'}>
+        <YMInitializer
+          accounts={[67444087]}
+          options={{
+            defer: true,
+            clickmap:true,
+            trackLinks:true,
+            accurateTrackBounce:true,
+            webvisor:true
+          }}
+        />
+        <Card title="Авторизация" style={{ maxWidth: 350 }} className={'login_card'}>
+          <Form onSubmit={this.handleSubmit} className="login_form">
 
-            <div className={'login_cont'}>
+            <Form.Item>
+              {getFieldDecorator('login', {
+                rules: [{ required: true, message: 'Необходимо указать Ваш логин' }],
+              })(
+                <Input
+                  prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                  placeholder="Введите логин"
+                />,
+              )}
+            </Form.Item>
 
-                <Card title="Авторизация" style={{ maxWidth: 350 }} className={'login_card'}>
-                    <Form onSubmit={this.handleSubmit} className="login_form">
+            <Form.Item>
+              {getFieldDecorator('password', {
+                rules: [{ required: true, message: 'Необходимо указать пароль' }],
+              })(
+                <Input
+                  prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                  type="password"
+                  placeholder="Введите пароль"
+                />,
+              )}
+            </Form.Item>
 
-                        <Form.Item>
-                            {getFieldDecorator('login', {
-                                rules: [{ required: true, message: 'Необходимо указать Ваш логин' }],
-                            })(
-                                <Input
-                                    prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                                    placeholder="Введите логин"
-                                />,
-                            )}
-                        </Form.Item>
+            <Form.Item>
+              {getFieldDecorator('remember', {
+                valuePropName: 'checked',
+                initialValue: true,
+              })(<Checkbox>Запомнить</Checkbox>)}
+              <Link to={'/rememberPass'} className="login_form_forgot">Забыл пароль?</Link>
+              <Button type="primary" htmlType="submit" className="login_form_button">
+                Войти
+              </Button>
+              Или <Link to={'/registration'}>зарегистрируйтесь</Link>
+            </Form.Item>
 
-                        <Form.Item>
-                            {getFieldDecorator('password', {
-                                rules: [{ required: true, message: 'Необходимо указать пароль' }],
-                            })(
-                                <Input
-                                    prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                                    type="password"
-                                    placeholder="Введите пароль"
-                                />,
-                            )}
-                        </Form.Item>
-
-                        <Form.Item>
-                            {getFieldDecorator('remember', {
-                                valuePropName: 'checked',
-                                initialValue: true,
-                            })(<Checkbox>Запомнить</Checkbox>)}
-                            <Link to={'/rememberPass'} className="login_form_forgot">Забыл пароль?</Link>
-                            <Button type="primary" htmlType="submit" className="login_form_button">
-                                Войти
-                            </Button>
-                            Или <Link to={'/registration'}>зарегистрируйтесь</Link>
-                        </Form.Item>
-
-                    </Form>
-                </Card>
-
-            </div>
-
-        );
-    }
+          </Form>
+        </Card>
+      </div>
+    );
+  }
 }
 
 const WrappedLogin = Form.create({ name: 'normal_login' })(Login);
