a6eaa3c903a21443586e294b50284cc698b3bf16
ilyaMah <ilya.unica@gmail.com>
2020-09-17 17:31:49 +0300

    heroku fix

--- backend/routes/loginRouter.js
+++ backend/routes/loginRouter.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-const bcrypt = require('bcrypt');
-
 router.get('/', async (req, res) => {
     res.json({test: 'hello bro!!!!!!'});
 });
